591827eb475baf271bf283282bdda0cda01df37d
sideroad <sideroad.jp@gmail.com>
2017-08-11 00:18:14 +0900

    Get from root path

--- static/pwabuilder-sw-register.js
+++ static/pwabuilder-sw-register.js
@@ -5,10 +5,9 @@ if (navigator.serviceWorker.controller) {
   console.log('[PWA Builder] active service worker found, no need to register')
 } else {
   //Register the ServiceWorker
-  navigator.serviceWorker.register('pwabuider-sw.js', {
+  navigator.serviceWorker.register('/pwabuider-sw.js', {
     scope: './'
   }).then(function(reg) {
     console.log('Service worker has been registered for scope:'+ reg.scope);
   });
 }
-
